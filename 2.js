c514f6ffbbfce99d75b53023dbfc4836a8ff2924
gozdeylmz95 <gozdeylmz95@gmail.com>
2019-11-15 22:01:30 +0300

    Add array method example

---
+++ 2.js
@@ -0,0 +1,8 @@
+let numbers = [1, 4, 9];
+
+
+let triples = numbers.map(item => {
+    return item * 3;
+})
+
+console.log(triples)
---
+++ 3.js
@@ -0,0 +1,7 @@
+let numbers = [12, 5, 20, 130, 44, 9, 240];
+
+let filteredNumbers = numbers.filter(item => {
+    return item < 15
+})
+
+console.log(filteredNumbers)
